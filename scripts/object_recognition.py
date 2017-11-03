8e940a0eb5b3fc3600110c6f0811607b16e60e8f
skreynolds <shane.k.reynolds@gmail.com>
2017-11-03 19:32:10 +0930

    Added the Voxel downsampling filter, the pass through filter, and the RANSAC plane segmentation - still have to implement the clustering

--- scripts/object_recognition.py
+++ scripts/object_recognition.py
@@ -16,7 +16,7 @@ from sensor_stick.marker_tools import *
 from sensor_stick.msg import DetectedObjectsArray
 from sensor_stick.msg import DetectedObject
 from sensor_stick.pcl_helper import *
-#from pcl_helper import *
+from pcl_helper import *
 
 def get_normals(cloud):
     get_normals_prox = rospy.ServiceProxy('/feature_extractor/get_normals', GetNormals)
@@ -27,25 +27,89 @@ def pcl_callback(pcl_msg):
 
 # Exercise-2 TODOs:
 
+##############################################################################
     # TODO: Convert ROS msg to PCL data
+##############################################################################
+    cloud = ros_to_pcl(pcl_msg)
 
+##############################################################################
     # TODO: Voxel Grid Downsampling
+##############################################################################
+    # Create a VoxelGrid filter object for our input point cloud
+    vox = cloud.make_voxel_grid_filter()
 
+    # Choose a voxel (also known as leaf) size
+    LEAF_SIZE = 0.01
+
+    # Set the voxel (or leaf) size
+    vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+
+    # Call the filter function to obtain the resultant downsampled point cloud
+    cloud_filtered = vox.filter()
+
+##############################################################################
     # TODO: PassThrough Filter
+##############################################################################
+    # Create a PassThrough filter object.
+    passthrough = cloud_filtered.make_passthrough_filter()
 
+    # Assign axis and range to the passthrough filter object.
+    filter_axis = 'z'
+    passthrough.set_filter_field_name(filter_axis)
+    axis_min = 0.75
+    axis_max = 1.1
+    passthrough.set_filter_limits(axis_min, axis_max)
+
+    # Finally use the filter function to obtain the resultant point cloud.
+    cloud_filtered = passthrough.filter()
+
+##############################################################################
     # TODO: RANSAC Plane Segmentation
+##############################################################################
+    # Create the segmentation object
+    seg = cloud_filtered.make_segmenter()
 
+    # Set the model you wish to fit
+    seg.set_model_type(pcl.SACMODEL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+
+    # Max distance for a point to be considered fitting the model
+    # Experiment with different values for max_distance
+    # for segmenting the table
+    max_distance = 0.02
+    seg.set_distance_threshold(max_distance)
+
+    # Call the segment function to obtain set of inlier indices and model coefficients
+    inliers, coefficients = seg.segment()
+
+##############################################################################
     # TODO: Extract inliers and outliers
+##############################################################################
+    # Extract inliers
+    cloud_table = cloud_filtered.extract(inliers, negative=False)
 
+    # Extract outliers
+    cloud_objects = cloud_filtered.extract(inliers, negative=True)
+
+##############################################################################
     # TODO: Euclidean Clustering
+##############################################################################
 
+##############################################################################
     # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+##############################################################################
 
+##############################################################################
     # TODO: Convert PCL data to ROS messages
+##############################################################################
+    ros_cloud_table = pcl_to_ros(cloud_table)
+    ros_cloud_objects = pcl_to_ros(cloud_objects)
 
+##############################################################################
     # TODO: Publish ROS messages
-    pcl_table_pub.publish(pcl_msg)
-    pcl_objects_pub.publish(pcl_msg)
+##############################################################################
+    pcl_table_pub.publish(ros_cloud_table)
+    pcl_objects_pub.publish(ros_cloud_objects)
     pcl_cluster_pub.publish(pcl_msg)
 
 # Exercise-3 TODOs:
